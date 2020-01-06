f2722229bb36120f9f9461b0d791dea698a1aabb
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-01-06 10:06:52 +0000

    Added sched_data_interface example

---
+++ examples/sched.py
@@ -0,0 +1,14 @@
+from secrets import SCHED_API_KEY
+
+import sys
+import os
+
+from urllib.parse import urlparse
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
+from sched_data_interface import SchedDataInterface
+
+if __name__ == "__main__":
+    data_interface = SchedDataInterface("https://linaroconnectsandiego.sched.com", SCHED_API_KEY, "SAN19")
+    sessions = data_interface.getSessionsData()
